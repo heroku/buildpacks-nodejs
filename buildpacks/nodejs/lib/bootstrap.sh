e3d4edbadae192f3f252ed3659b30d9dda15057c
Danielle Adams <danielle.adams@salesforce.com>
2021-01-19 12:12:51 -0500

    add shellcheck disables for lib scripts

--- buildpacks/nodejs/lib/bootstrap.sh
+++ buildpacks/nodejs/lib/bootstrap.sh
@@ -2,6 +2,7 @@
 
 set -eo pipefail
 
+# shellcheck disable=SC2128
 bp_dir=$(
 	cd "$(dirname "$BASH_SOURCE")" || exit
 	cd ..
--- buildpacks/nodejs/lib/build.sh
+++ buildpacks/nodejs/lib/build.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# shellcheck disable=SC2128
 bp_dir=$(
 	cd "$(dirname "$BASH_SOURCE")" || exit
 	cd ..
