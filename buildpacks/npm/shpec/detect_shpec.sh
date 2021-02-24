d6dd1d8e9ecfcf649e14a2e1b7aff784fbf066ad
Danielle Adams <danielle.adams@salesforce.com>
2021-02-24 15:25:34 -0500

    remove rogue output

--- buildpacks/npm/shpec/detect_shpec.sh
+++ buildpacks/npm/shpec/detect_shpec.sh
@@ -39,10 +39,11 @@ describe "lib/detect.sh"
 	describe "write_to_build_plan"
 		it "writes node and node_modules as expected in build plan"
 			project_dir=$(create_temp_project_dir)
+
 			touch "$project_dir/buildplan.toml"
 			write_to_build_plan "$project_dir/buildplan.toml"
+
 			actual=$(cat "$project_dir/buildplan.toml")
-			echo "$actual"
 			expected=$(cat <<EOF
 	[[provides]]
 	name = "node_modules"
