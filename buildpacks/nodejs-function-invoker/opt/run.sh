99b066a696627a721cee44b05c2af6f86d3b70f6
Ed Morley <501702+edmorley@users.noreply.github.com>
2021-05-12 19:11:06 +0100

    function-invoker: Fix unbound variable error when using DEBUG_PORT
    If `DEBUG_PORT` is defined, but `NODE_OPTIONS` was not already set,
    the functions invocation would fail with:
    
    ```
    ...
    Running on port :8080
    Debugger running on port :9229
    /layers/heroku_nodejs-function-invoker/opt/run.sh: line 9: NODE_OPTIONS: unbound variable
    ```
    
    Follow-up to #59.
    
    Refs GUS-W-9260143.

--- buildpacks/nodejs-function-invoker/opt/run.sh
+++ buildpacks/nodejs-function-invoker/opt/run.sh
@@ -6,7 +6,7 @@ app_dir="${1:?}"
 
 if [[ -n "${DEBUG_PORT:-}" ]]; then
 	# Adds a space separator only if NODE_OPTIONS was already set.
-	export NODE_OPTIONS="${NODE_OPTIONS}${NODE_OPTIONS:+ }--inspect=0.0.0.0:${DEBUG_PORT}"
+	export NODE_OPTIONS="${NODE_OPTIONS:-}${NODE_OPTIONS:+ }--inspect=0.0.0.0:${DEBUG_PORT}"
 fi
 
 exec sf-fx-runtime-nodejs serve "${app_dir}" --host 0.0.0.0 --port "${PORT:-8080}"
