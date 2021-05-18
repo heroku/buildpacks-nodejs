9f674bee65cf60e2b0f88999487c864cb8b8e5a7
Danielle Adams <6271256+danielleadams@users.noreply.github.com>
2021-05-18 15:16:23 -0400

    install yj before detect script (#66)
    * install yj before detect script
    
    * update CHANGELOG; fix shellcheck'
    
    * move downloaded bins to downloads dir

--- buildpacks/nodejs-function-invoker/lib/utils/download.sh
+++ buildpacks/nodejs-function-invoker/lib/utils/download.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+download_yj() {
+	bp_dir=$1
+
+	mkdir -p "${bp_dir}/downloads/bin"
+
+	if [[ ! -f "${bp_dir}/downloads/bin/yj" ]]; then
+		curl -Ls https://github.com/sclevine/yj/releases/download/v2.0/yj-linux >"${bp_dir}/downloads/bin/yj" &&
+			chmod +x "${bp_dir}/downloads/bin/yj"
+	fi
+}
+
 download_file() {
 	local -r url="${1:?}"
 	local -r target_path="${2:?}"
