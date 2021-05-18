00410694147df5a807d41c09b6048c13d4d9205d
Danielle Adams <danielle.adams@salesforce.com>
2021-05-18 14:20:42 -0400

    install yj before detect script

--- buildpacks/nodejs-function-invoker/lib/utils/download.sh
+++ buildpacks/nodejs-function-invoker/lib/utils/download.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+install_yj() {
+	build_dir=$1
+
+	mkdir -p "${build_dir}/bin"
+
+	if [[ ! -f "${build_dir}/bin/yj" ]]; then
+		curl -Ls https://github.com/sclevine/yj/releases/download/v2.0/yj-linux >"${build_dir}/bin/yj" &&
+			chmod +x "${build_dir}/bin/yj"
+	fi
+}
+
 download_file() {
 	local -r url="${1:?}"
 	local -r target_path="${2:?}"
