e75124cc32317b5b45964b294d544153c43d8d42
Richard Schneeman <rschneeman@salesforce.com>
2021-07-21 14:48:50 -0500

    Add cutlass tests for TypeScript support
    Work item: https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07AH000000OAyJYAW/view
    
    This PR introduces a test app `simple-typescript-function` that contains necessary files to activate the typescript support of this project https://github.com/heroku/buildpacks-nodejs/blob/53ac19086d0959c50e3d97128d62760816e753f4/buildpacks/typescript/lib/detect.sh.
    
    The ultimate purpose of exercising this code is to verify that types are correctly imported (https://github.com/forcedotcom/sf-fx-sdk-nodejs/pull/79) and can be used by a customer's function.
    
    I'm also updating the tests to not run in parallel due to a race condition in tearing down the "local buildpack". More info: https://gus.lightning.force.com/lightning/r/ADM_Work__c/a07AH000000Roc2YAC/view

--- test/specs/node-function/cutlass/function_spec.rb
+++ test/specs/node-function/cutlass/function_spec.rb
@@ -21,4 +21,23 @@ describe "Heroku's Nodejs CNB" do
       end
     end
   end
+
+  it "generates a callable salesforce function from typescript" do
+    Cutlass::App.new("simple-typescript-function").transaction do |app|
+      app.pack_build do |pack_result|
+        expect(pack_result.stdout).to include("Installing Node.js function runtime")
+      end
+
+      app.start_container(expose_ports: 8080) do |container|
+        body = { }
+        query = Cutlass::FunctionQuery.new(
+          port: container.get_host_port(8080),
+          body: body
+        ).call
+
+        expect(query.as_json).to eq("hello world from typescript")
+        expect(query.success?).to be_truthy
+      end
+    end
+  end
 end
