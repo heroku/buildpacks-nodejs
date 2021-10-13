5d6fcafcaf603f0fb8c60f9fa78fffc652b78ebf
Josh W Lewis <josh.lewis@salesforce.com>
2021-10-13 11:26:09 -0500

    Prefer --debug-port over NODE_OPTIONS=--inspect
    sf-fx-runtime-nodejs@0.9.0 manages inspection options itself.

--- buildpacks/nodejs-function-invoker/opt/run.sh
+++ buildpacks/nodejs-function-invoker/opt/run.sh
@@ -4,9 +4,4 @@ set -euo pipefail
 
 app_dir="${1:?}"
 
-if [[ -n "${DEBUG_PORT:-}" ]]; then
-	# Adds a space separator only if NODE_OPTIONS was already set.
-	export NODE_OPTIONS="${NODE_OPTIONS:-}${NODE_OPTIONS:+ }--inspect=0.0.0.0:${DEBUG_PORT}"
-fi
-
-exec sf-fx-runtime-nodejs serve "${app_dir}" --host 0.0.0.0 --port "${PORT:-8080}"
+exec sf-fx-runtime-nodejs serve "${app_dir}" --host=0.0.0.0 --port="${PORT:-8080}" --debug-port="${DEBUG_PORT}"
