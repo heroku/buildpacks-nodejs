d4f52ac58dae79e7715db8a541c99ff1dd6378fe
Josh W Lewis <josh.lewis@salesforce.com>
2021-10-13 12:09:18 -0500

    Set DEBUG_PORT fallback to ''

--- buildpacks/nodejs-function-invoker/opt/run.sh
+++ buildpacks/nodejs-function-invoker/opt/run.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 app_dir="${1:?}"
 
-exec sf-fx-runtime-nodejs serve "${app_dir}" --host=0.0.0.0 --port="${PORT:-8080}" --debug-port="${DEBUG_PORT}"
+exec sf-fx-runtime-nodejs serve "${app_dir}" --host=0.0.0.0 --port="${PORT:-8080}" --debug-port="${DEBUG_PORT:-}"
