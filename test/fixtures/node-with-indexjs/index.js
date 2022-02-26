03bc820d1cb8eeb81e50858971c131c3696b1c26
Josh W Lewis <josh.lewis@salesforce.com>
2022-02-26 17:23:50 -0600

    Add integration tests for nodejs-runtime

---
+++ test/fixtures/node-with-indexjs/index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const port = process.env["PORT"] || 8080;
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello from node-with-indexjs!');
+}
+
+const server = http.createServer(requestListener);
+server.listen(port);
