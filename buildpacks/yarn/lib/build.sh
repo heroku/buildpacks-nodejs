c582187c5182e71ab485918ca417f6e3f2372c4a
Josh W Lewis <josh.lewis@salesforce.com>
2022-03-02 15:10:03 -0600

    Include [types] table for the toolbox

--- buildpacks/yarn/lib/build.sh
+++ buildpacks/yarn/lib/build.sh
@@ -57,7 +57,8 @@ install_or_reuse_toolbox() {
 			chmod +x "${layer_dir}/bin/yj"
 	fi
 
-	echo "cache = true" >"${layer_dir}.toml"
+	echo "[types]" > "${layer_dir}.toml"
+	echo "cache = true" >>"${layer_dir}.toml"
 	echo "build = true" >>"${layer_dir}.toml"
 	echo "launch = false" >>"${layer_dir}.toml"
 }
