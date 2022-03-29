b7d939c5aa25cf6cda76eb77053da9714d914768
Josh W Lewis <josh.lewis@salesforce.com>
2022-03-29 18:47:09 -0500

    Add build.sh script for nodejs-function-invoker

---
+++ buildpacks/nodejs-function-invoker/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+buildpack_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+pushd "${buildpack_dir}"
+
+cargo libcnb package --release
+
+rm -rf target
+cp -R ../../target/buildpack/release/heroku_nodejs-function-invoker target
+cp package.toml target/
+
+popd
