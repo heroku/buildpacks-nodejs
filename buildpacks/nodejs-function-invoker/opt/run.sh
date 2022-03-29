844a0d5c75832bb697756657b292eaf4d62b9508
Josh W Lewis <josh.lewis@salesforce.com>
2022-03-29 12:47:13 -0500

    Drop nodejs-function-invoker bash implementation

--- buildpacks/nodejs-function-invoker/opt/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-app_dir="${1:?}"
-
-exec sf-fx-runtime-nodejs serve "${app_dir}" --host=0.0.0.0 --port="${PORT:-8080}" --workers=2 --debug-port="${DEBUG_PORT:-}"
