7db8afcce12b9ce33342a13d594193b2a8e05fff
Josh W Lewis <josh.lewis@salesforce.com>
2022-08-15 17:30:58 -0500

    Add a stack upgrade test for a node function

--- buildpacks/nodejs-engine/tests/stack_change_test.rs
+++ buildpacks/nodejs-engine/tests/stack_change_test.rs
@@ -3,31 +3,6 @@
 use libcnb_test::{assert_contains, BuildConfig, ContainerConfig, TestRunner};
 use std::time::Duration;
 
-fn test_node(fixture: &str, builder: &str, expect_lines: &[&str]) {
-    TestRunner::default().build(
-        BuildConfig::new(builder, format!("../../test/fixtures/{fixture}")),
-        |ctx| {
-            for expect_line in expect_lines {
-                assert_contains!(ctx.pack_stdout, expect_line);
-            }
-            let port = 8080;
-            ctx.start_container(ContainerConfig::new().expose_port(port), |container| {
-                std::thread::sleep(Duration::from_secs(1));
-                let addr = container
-                    .address_for_port(port)
-                    .expect("couldn't get container address");
-                let resp = ureq::get(&format!("http://{addr}"))
-                    .call()
-                    .expect("request to container failed")
-                    .into_string()
-                    .expect("response read error");
-
-                assert_contains!(resp, fixture);
-            });
-        },
-    );
-}
-
 #[test]
 #[ignore]
 fn upgrade_simple_indexjs_from_heroku20_to_heroku22() {
