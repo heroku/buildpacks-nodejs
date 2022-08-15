1ba0f19d555daabe9b6d35a5f4e563052d66aab8
Josh W Lewis <josh.lewis@salesforce.com>
2022-08-15 12:57:07 -0500

    Test nodejs-engine on heroku-22

--- buildpacks/nodejs-engine/tests/integration_test.rs
+++ buildpacks/nodejs-engine/tests/integration_test.rs
@@ -3,17 +3,13 @@
 use libcnb_test::{assert_contains, BuildConfig, ContainerConfig, TestRunner};
 use std::time::Duration;
 
-#[test]
-#[ignore]
-fn test_node_with_indexjs() {
+fn test_node(fixture: &str, builder: &str, expect_lines: &[&str]) {
     TestRunner::default().build(
-        BuildConfig::new(
-            "heroku/buildpacks:20",
-            "../../test/fixtures/node-with-indexjs",
-        ),
+        BuildConfig::new(builder, format!("../../test/fixtures/{fixture}")),
         |ctx| {
-            assert_contains!(ctx.pack_stdout, "Detected Node.js version range: *");
-            assert_contains!(ctx.pack_stdout, "Installing Node.js");
+            for expect_line in expect_lines {
+                assert_contains!(ctx.pack_stdout, expect_line);
+            }
             let port = 8080;
             ctx.start_container(ContainerConfig::new().expose_port(port), |container| {
                 std::thread::sleep(Duration::from_secs(1));
@@ -26,7 +22,7 @@ fn test_node_with_indexjs() {
                     .into_string()
                     .expect("response read error");
 
-                assert_contains!(resp, "node-with-indexjs");
+                assert_contains!(resp, fixture);
             });
         },
     );
@@ -34,27 +30,24 @@ fn test_node_with_indexjs() {
 
 #[test]
 #[ignore]
-fn test_node_with_serverjs() {
-    TestRunner::default().build(
-        BuildConfig::new(
-            "heroku/buildpacks:20",
-            "../../test/fixtures/node-with-serverjs",
-        ),
-        |ctx| {
-            assert_contains!(ctx.pack_stdout, "Installing Node.js 16.0.0");
-            let port = 8080;
-            ctx.start_container(ContainerConfig::new().expose_port(port), |container| {
-                std::thread::sleep(Duration::from_secs(1));
-                let addr = container
-                    .address_for_port(port)
-                    .expect("couldn't get container address");
-                let resp = ureq::get(&format!("http://{addr}"))
-                    .call()
-                    .expect("request to container failed")
-                    .into_string()
-                    .expect("response read error");
-                assert_contains!(resp, "node-with-serverjs");
-            });
-        },
-    );
+fn indexjs_20() {
+    test_node("node-with-indexjs", "heroku/buildpacks:20", &["Detected Node.js version range: *", "Installing Node.js"]);
+}
+
+#[test]
+#[ignore]
+fn indexjs_22() {
+    test_node("node-with-indexjs", "heroku/builder:22", &["Detected Node.js version range: *", "Installing Node.js"]);
+}
+
+#[test]
+#[ignore]
+fn serverjs_20() {
+    test_node("node-with-serverjs", "heroku/buildpacks:20", &["Installing Node.js 16.0.0"]);
+}
+
+#[test]
+#[ignore]
+fn serverjs_22() {
+    test_node("node-with-serverjs", "heroku/builder:22", &["Installing Node.js 16.0.0"]);
 }
