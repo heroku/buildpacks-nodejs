419c8ccc301a794da1ef110893aa1f23c20aa415
Josh W Lewis <josh.lewis@salesforce.com>
2022-11-16 18:15:25 -0600

    Invalidate yarn deps cache after 150 builds

--- buildpacks/nodejs-yarn/src/layers/deps.rs
+++ buildpacks/nodejs-yarn/src/layers/deps.rs
@@ -1,7 +1,6 @@
 use crate::yarn::Yarn;
 use crate::{NodeJsYarnBuildpack, NodeJsYarnBuildpackError};
 use libcnb::build::BuildContext;
-use libcnb::data::buildpack::StackId;
 use libcnb::data::layer_content_metadata::LayerTypes;
 use libcnb::layer::{ExistingLayerStrategy, Layer, LayerData, LayerResult, LayerResultBuilder};
 use libcnb::Buildpack;
@@ -13,16 +12,19 @@ use thiserror::Error;
 
 /// `DepsLayer` is a layer for caching yarn dependencies from build to build.
 /// This layer is irrelevant in zero-install mode, as cached dependencies are
-/// included in the source code.
+/// included in the source code. Yarn only prunes unused dependencies in a few
+/// scenarios, so the cache is invalidated after a number of builds to prevent
+/// unbound cache growth.
 pub(crate) struct DepsLayer {
     pub(crate) yarn: Yarn,
 }
 
-#[derive(Deserialize, Serialize, Clone, PartialEq, Eq)]
+#[derive(Deserialize, Serialize, Clone, PartialEq)]
 pub(crate) struct DepsLayerMetadata {
-    yarn: String,
+    // Using float here due to [an issue with lifecycle's handling of integers](https://github.com/buildpacks/lifecycle/issues/884)
+    cache_usage_count: f32,
     layer_version: String,
-    stack_id: StackId,
+    yarn: String,
 }
 
 #[derive(Error, Debug)]
@@ -30,6 +32,8 @@ pub(crate) struct DepsLayerMetadata {
 pub(crate) struct DepsLayerError(std::io::Error);
 
 const LAYER_VERSION: &str = "1";
+const MAX_CACHE_USAGE_COUNT: f32 = 150.0;
+const CACHE_DIR: &str = "cache";
 
 impl Layer for DepsLayer {
     type Buildpack = NodeJsYarnBuildpack;
@@ -45,25 +49,33 @@ impl Layer for DepsLayer {
 
     fn create(
         &self,
-        context: &BuildContext<Self::Buildpack>,
+        _context: &BuildContext<Self::Buildpack>,
         layer_path: &Path,
     ) -> Result<LayerResult<Self::Metadata>, NodeJsYarnBuildpackError> {
-        fs::create_dir(layer_path.join("cache")).map_err(DepsLayerError)?;
-        LayerResultBuilder::new(DepsLayerMetadata::new(self, context)).build()
+        fs::create_dir(layer_path.join(CACHE_DIR)).map_err(DepsLayerError)?;
+        LayerResultBuilder::new(DepsLayerMetadata::new(self)).build()
+    }
+
+    fn update(
+        &self,
+        _context: &BuildContext<Self::Buildpack>,
+        layer: &LayerData<Self::Metadata>,
+    ) -> Result<LayerResult<Self::Metadata>, NodeJsYarnBuildpackError> {
+        LayerResultBuilder::new(DepsLayerMetadata {
+            cache_usage_count: layer.content_metadata.metadata.cache_usage_count + 1.0,
+            ..DepsLayerMetadata::new(self)
+        })
+        .build()
     }
 
     fn existing_layer_strategy(
         &self,
-        context: &BuildContext<Self::Buildpack>,
+        _context: &BuildContext<Self::Buildpack>,
         layer_data: &LayerData<Self::Metadata>,
     ) -> Result<ExistingLayerStrategy, <Self::Buildpack as Buildpack>::Error> {
-        if layer_data
-            .content_metadata
-            .metadata
-            .is_cacheable(self, context)
-        {
+        if layer_data.content_metadata.metadata.is_reusable(self) {
             log_info("Restoring yarn dependency cache");
-            Ok(ExistingLayerStrategy::Keep)
+            Ok(ExistingLayerStrategy::Update)
         } else {
             log_info("Clearing yarn dependency cache");
             Ok(ExistingLayerStrategy::Recreate)
@@ -72,17 +84,17 @@ impl Layer for DepsLayer {
 }
 
 impl DepsLayerMetadata {
-    fn is_cacheable(&self, layer: &DepsLayer, context: &BuildContext<NodeJsYarnBuildpack>) -> bool {
-        self.yarn == layer.yarn.to_string()
-            && self.stack_id == context.stack_id
+    fn is_reusable(&self, new_layer: &DepsLayer) -> bool {
+        self.yarn == new_layer.yarn.to_string()
             && self.layer_version == *LAYER_VERSION
+            && self.cache_usage_count < MAX_CACHE_USAGE_COUNT
     }
 
-    fn new(layer: &DepsLayer, context: &BuildContext<NodeJsYarnBuildpack>) -> Self {
+    fn new(layer: &DepsLayer) -> Self {
         DepsLayerMetadata {
             yarn: layer.yarn.to_string(),
-            stack_id: context.stack_id.clone(),
             layer_version: LAYER_VERSION.to_string(),
+            cache_usage_count: 1.0,
         }
     }
 }
