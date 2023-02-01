24c51793ccd5473242d04ff2ca5e75bb66362f92
Josh W Lewis <josh.lewis@salesforce.com>
2023-02-01 12:33:53 -0600

    Fix clippy issue on newer rust

--- buildpacks/nodejs-yarn/src/main.rs
+++ buildpacks/nodejs-yarn/src/main.rs
@@ -82,8 +82,7 @@ impl Buildpack for YarnBuildpack {
                     }
                     Some(requirement) => {
                         log_info(format!(
-                            "Detected yarn version range {} from package.json",
-                            requirement
+                            "Detected yarn version range {requirement} from package.json"
                         ));
                         requirement
                     }
