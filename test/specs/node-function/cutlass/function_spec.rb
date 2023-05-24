38ac8e6878f7f7779e4a4cd394492d28f5070075
Josh W Lewis <josh.lewis@salesforce.com>
2023-05-24 11:00:44 -0500

    Fix spec syntax error

--- test/specs/node-function/cutlass/function_spec.rb
+++ test/specs/node-function/cutlass/function_spec.rb
@@ -24,23 +24,23 @@ describe "Heroku's Nodejs CNB" do
         expect(container.logs.stdout).to include("logging info is a fun 1")
       end
     end
+  end
 
-    it "generates a callable salesforce function from typescript" do
-      Cutlass::App.new("simple-typescript-function", builder: builder).transaction do |app|
-        app.pack_build do |pack_result|
-          expect(pack_result.stdout).to include("Installing Node.js Function Invoker")
-        end
+  it "generates a callable salesforce function from typescript" do
+    Cutlass::App.new("simple-typescript-function", builder: builder).transaction do |app|
+      app.pack_build do |pack_result|
+        expect(pack_result.stdout).to include("Installing Node.js Function Invoker")
+      end
 
-        app.start_container(expose_ports: 8080, memory: 1e9) do |container|
-          body = { }
-          query = Cutlass::FunctionQuery.new(
-            port: container.get_host_port(8080),
-            body: body
-          ).call
+      app.start_container(expose_ports: 8080, memory: 1e9) do |container|
+        body = { }
+        query = Cutlass::FunctionQuery.new(
+          port: container.get_host_port(8080),
+          body: body
+        ).call
 
-          expect(query.as_json).to eq("hello world from typescript")
-          expect(query.success?).to be_truthy
-        end
+        expect(query.as_json).to eq("hello world from typescript")
+        expect(query.success?).to be_truthy
       end
     end
   end
