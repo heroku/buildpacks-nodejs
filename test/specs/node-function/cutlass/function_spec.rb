d97613d991cb417f27af273030f1cfb1f9fac01f
Josh W Lewis <josh.lewis@salesforce.com>
2023-05-24 10:53:51 -0500

    Drop heroku/buildpacks:20 cutlass tests for functions

--- test/specs/node-function/cutlass/function_spec.rb
+++ test/specs/node-function/cutlass/function_spec.rb
@@ -3,45 +3,43 @@
 require_relative "../spec_helper"
 
 describe "Heroku's Nodejs CNB" do
-  ["heroku/buildpacks:20", "heroku/builder:22"].each do |builder|
-    describe "with #{builder}" do
-      it "generates a callable salesforce function" do
-        Cutlass::App.new("simple-function", builder: builder).transaction do |app|
-          app.pack_build do |pack_result|
-            expect(pack_result.stdout).to include("Installing Node.js Function Invoker")
-          end
+  let(:builder) { "heroku/builder:22" }
 
-          app.start_container(expose_ports: 8080, memory: 1e9) do |container|
-            body = { }
-            query = Cutlass::FunctionQuery.new(
-              port: container.get_host_port(8080),
-              body: body
-            ).call
-
-            expect(query.as_json).to eq("hello world")
-            expect(query.success?).to be_truthy
-
-            expect(container.logs.stdout).to include("logging info is a fun 1")
-          end
-        end
+  it "generates a callable salesforce function" do
+    Cutlass::App.new("simple-function", builder: builder).transaction do |app|
+      app.pack_build do |pack_result|
+        expect(pack_result.stdout).to include("Installing Node.js Function Invoker")
       end
 
-      it "generates a callable salesforce function from typescript" do
-        Cutlass::App.new("simple-typescript-function", builder: builder).transaction do |app|
-          app.pack_build do |pack_result|
-            expect(pack_result.stdout).to include("Installing Node.js Function Invoker")
-          end
+      app.start_container(expose_ports: 8080, memory: 1e9) do |container|
+        body = { }
+        query = Cutlass::FunctionQuery.new(
+          port: container.get_host_port(8080),
+          body: body
+        ).call
 
-          app.start_container(expose_ports: 8080, memory: 1e9) do |container|
-            body = { }
-            query = Cutlass::FunctionQuery.new(
-              port: container.get_host_port(8080),
-              body: body
-            ).call
+        expect(query.as_json).to eq("hello world")
+        expect(query.success?).to be_truthy
 
-            expect(query.as_json).to eq("hello world from typescript")
-            expect(query.success?).to be_truthy
-          end
+        expect(container.logs.stdout).to include("logging info is a fun 1")
+      end
+    end
+
+    it "generates a callable salesforce function from typescript" do
+      Cutlass::App.new("simple-typescript-function", builder: builder).transaction do |app|
+        app.pack_build do |pack_result|
+          expect(pack_result.stdout).to include("Installing Node.js Function Invoker")
+        end
+
+        app.start_container(expose_ports: 8080, memory: 1e9) do |container|
+          body = { }
+          query = Cutlass::FunctionQuery.new(
+            port: container.get_host_port(8080),
+            body: body
+          ).call
+
+          expect(query.as_json).to eq("hello world from typescript")
+          expect(query.success?).to be_truthy
         end
       end
     end
