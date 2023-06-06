1135522b0024ff4aac7a7a6518637832e4192165
Josh W Lewis <josh.lewis@salesforce.com>
2023-06-06 09:28:03 -0500

    Update script to pass shellcheck

--- buildpacks/nodejs-function-invoker/opt/nodejs-runtime.sh
+++ buildpacks/nodejs-function-invoker/opt/nodejs-runtime.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 runtime_bin=$1
 function_dir=$2
 
-exec $runtime_bin serve $function_dir --workers 2 --host "::" --port "${PORT:-8080}" --debug-port "${DEBUG_PORT:-}"
+exec $runtime_bin serve "$function_dir" --workers 2 --host "::" --port "${PORT:-8080}" --debug-port "${DEBUG_PORT:-}"
