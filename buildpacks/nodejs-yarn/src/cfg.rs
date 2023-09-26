ab66e5aead7f168fba0b06ccb0ecc38edf206ee8
Colin Casey <casey.colin@gmail.com>
2023-09-26 18:09:28 -0300

    Fixed some test references

--- buildpacks/nodejs-yarn/src/cfg.rs
+++ buildpacks/nodejs-yarn/src/cfg.rs
@@ -30,7 +30,7 @@ mod tests {
     #[test]
     fn test_cache_populated() {
         let path = Path::new(env!("CARGO_MANIFEST_DIR"))
-            .join("../../test/fixtures/yarn-3-modules-zero/.yarn/cache");
+            .join("./tests/fixtures/yarn-3-modules-zero/.yarn/cache");
         assert!(
             cache_populated(&path),
             "Expected zero-install app to have a populated cache"
