5f97cc8417d9be41126f86e4641288d0f5529058
Josh W Lewis <josh.lewis@salesforce.com>
2023-10-24 11:18:58 -0500

    Adjust installation assertion for yarn 4 logging

--- buildpacks/nodejs-yarn/tests/integration_test.rs
+++ buildpacks/nodejs-yarn/tests/integration_test.rs
@@ -75,12 +75,9 @@ fn yarn_4_pnp_nonzero() {
         assert_contains!(ctx.pack_stdout, "Installing yarn");
         assert_contains!(ctx.pack_stdout, "Installing dependencies");
         assert_contains!(ctx.pack_stdout, "Successfully set cacheFolder");
-        assert_contains!(
-            ctx.pack_stdout,
-            "can't be found in the cache and will be fetched from the remote registry"
-        );
         assert_contains!(ctx.pack_stdout, "Resolution step");
         assert_contains!(ctx.pack_stdout, "Fetch step");
+        assert_contains!(ctx.pack_stdout, "62 packages were added to the project");
         assert_contains!(ctx.pack_stdout, "Link step");
         assert_contains!(ctx.pack_stdout, "Completed");
         assert_web_response(&ctx, "yarn-4-pnp-nonzero");
