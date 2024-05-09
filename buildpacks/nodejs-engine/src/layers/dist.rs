aeb50fe0efce3c219fbc79fd8df1121516442792
Rune Soerensen <rsoerensen@salesforce.com>
2024-05-08 21:04:25 -0400

    Use artifact inventory (#814)
    * Implement VersionRequirement for Requirement
    
    * Use Artifact in nodejs-engine
    
    * Update inventory.toml with new artifact format
    
    Logic for updating this from the nodejs.org feed will be added later
    
    * Rename release to artifact
    
    * Use Artifact directory for node DistLayerMetaData
    
    * Prefer Artifact#Display over Version#Display
    
    * Test output displays os and arch
    
    * Print download source to buildpack output
    
    * Update go buildpack revision
    
    * Generalize tarball prefix extraction
    
    Node tarballs are prefixed with the filename sans ".tar.gz", so this will allow extraction of arm64 artifacts as well.
    
    * Verify checksum
    
    * Resolve node artifact based on current os/arch
    
    * Fix typo
    
    * Reduce verbosity
    
    * Simplify expression
    
    * Add bin/update_node_inventory
    
    * Update GitHub action for new inventory
    
    * Reduce visibility
    
    * Remove unnecessary comment
    
    * Rename remote_artifacts to upstream_artifacts
    
    * Fix error message
    
    * Update comment
    
    * Update earliest version variable name
    
    * Prefer three member tuple
    
    * Use exit code 1 consistently
    
    Non-zero exit code values aren't used anywhere
    
    * Remove unnecessary qualifier
    
    * Simplify expression
    
    * Prefer updating rust toolchain directly
    
    Avoid dependency on an archived github action, and take the same approach as other workflows in this repository
    
    * Use Rust cache
    
    * Simplify method
    
    It's clear what is happening here, so avoid assigning an "earliest version" variable/providing context for an error that'll be trivial to identify if it's ever changed.
    
    * Use Result rather than custom process exits
    
    When an error is encountered, the process will still be terminated with a non-zero exit code with adequate error information for debugging an issue written to stderr
    
    * Inline list_upstream_artifacts logic in main fn
    
    * Inline get_release_artifacts function
    
    * Clone existing inventory artifacts
    
    * Simplify expression

--- buildpacks/nodejs-engine/src/layers/dist.rs
+++ buildpacks/nodejs-engine/src/layers/dist.rs
@@ -1,5 +1,6 @@
 use crate::{NodeJsEngineBuildpack, NodeJsEngineBuildpackError};
-use heroku_nodejs_utils::inv::Release;
+use heroku_inventory_utils::inv::Artifact;
+use heroku_nodejs_utils::vrs::Version;
 use libcnb::build::BuildContext;
 use libcnb::data::layer_content_metadata::LayerTypes;
 use libcnb::layer::{ExistingLayerStrategy, Layer, LayerData, LayerResult, LayerResultBuilder};
@@ -9,21 +10,22 @@ use libherokubuildpack::fs::move_directory_contents;
 use libherokubuildpack::log::log_info;
 use libherokubuildpack::tar::decompress_tarball;
 use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
+use std::fs;
+use std::io::Read;
 use std::path::Path;
 use tempfile::NamedTempFile;
 use thiserror::Error;
 
 /// A layer that downloads the Node.js distribution artifacts
 pub(crate) struct DistLayer {
-    pub(crate) release: Release,
+    pub(crate) artifact: Artifact<Version, Sha256>,
 }
 
 #[derive(Deserialize, Serialize, Clone, PartialEq, Eq)]
 pub(crate) struct DistLayerMetadata {
+    artifact: Artifact<Version, Sha256>,
     layer_version: String,
-    nodejs_version: String,
-    arch: String,
-    os: String,
 }
 
 #[derive(Error, Debug)]
@@ -34,8 +36,14 @@ pub(crate) enum DistLayerError {
     Download(libherokubuildpack::download::DownloadError),
     #[error("Couldn't decompress Node.js distribution: {0}")]
     Untar(std::io::Error),
+    #[error("Couldn't extract tarball prefix from artifact URL: {0}")]
+    TarballPrefix(String),
     #[error("Couldn't move Node.js distribution artifacts to the correct location: {0}")]
     Installation(std::io::Error),
+    #[error("Error verifying checksum")]
+    ChecksumVerification,
+    #[error("Couldn't read tempfile for Node.js distribution: {0}")]
+    ReadTempFile(std::io::Error),
 }
 
 const LAYER_VERSION: &str = "1";
@@ -54,32 +62,43 @@ impl Layer for DistLayer {
 
     fn create(
         &mut self,
-        context: &BuildContext<Self::Buildpack>,
+        _context: &BuildContext<Self::Buildpack>,
         layer_path: &Path,
     ) -> Result<LayerResult<Self::Metadata>, NodeJsEngineBuildpackError> {
         let node_tgz = NamedTempFile::new().map_err(DistLayerError::TempFile)?;
 
-        log_info(format!("Downloading Node.js {}", self.release.version));
-        download_file(&self.release.url, node_tgz.path()).map_err(DistLayerError::Download)?;
+        log_info(format!(
+            "Downloading Node.js {} from {}",
+            self.artifact, self.artifact.url
+        ));
+        download_file(&self.artifact.url, node_tgz.path()).map_err(DistLayerError::Download)?;
 
-        log_info(format!("Extracting Node.js {}", self.release.version));
+        log_info("Verifying checksum");
+        let digest = sha256(node_tgz.path()).map_err(DistLayerError::ReadTempFile)?;
+        if self.artifact.checksum.value != digest {
+            Err(DistLayerError::ChecksumVerification)?;
+        }
+
+        log_info(format!("Extracting Node.js {}", self.artifact));
         decompress_tarball(&mut node_tgz.into_file(), layer_path).map_err(DistLayerError::Untar)?;
 
-        log_info(format!("Installing Node.js {}", self.release.version));
-        let dist_name = format!("node-v{}-{}", self.release.version, "linux-x64");
+        log_info(format!("Installing Node.js {}", self.artifact));
+
+        let dist_name = extract_tarball_prefix(&self.artifact.url)
+            .ok_or_else(|| DistLayerError::TarballPrefix(self.artifact.url.clone()))?;
         let dist_path = Path::new(layer_path).join(dist_name);
         move_directory_contents(dist_path, layer_path).map_err(DistLayerError::Installation)?;
 
-        LayerResultBuilder::new(DistLayerMetadata::current(self, context)).build()
+        LayerResultBuilder::new(DistLayerMetadata::current(self)).build()
     }
 
     fn existing_layer_strategy(
         &mut self,
-        context: &BuildContext<Self::Buildpack>,
+        _context: &BuildContext<Self::Buildpack>,
         layer_data: &LayerData<Self::Metadata>,
     ) -> Result<ExistingLayerStrategy, <Self::Buildpack as Buildpack>::Error> {
-        if layer_data.content_metadata.metadata == DistLayerMetadata::current(self, context) {
-            log_info(format!("Reusing Node.js {}", self.release.version));
+        if layer_data.content_metadata.metadata == DistLayerMetadata::current(self) {
+            log_info(format!("Reusing Node.js {}", self.artifact));
             Ok(ExistingLayerStrategy::Keep)
         } else {
             Ok(ExistingLayerStrategy::Recreate)
@@ -87,13 +106,32 @@ impl Layer for DistLayer {
     }
 }
 
+fn sha256(path: impl AsRef<Path>) -> Result<Vec<u8>, std::io::Error> {
+    let mut file = fs::File::open(path.as_ref())?;
+    let mut buffer = [0x00; 10 * 1024];
+    let mut sha256: Sha256 = sha2::Sha256::default();
+
+    let mut read = file.read(&mut buffer)?;
+    while read > 0 {
+        Digest::update(&mut sha256, &buffer[..read]);
+        read = file.read(&mut buffer)?;
+    }
+
+    Ok(sha256.finalize().to_vec())
+}
+
+fn extract_tarball_prefix(url: &str) -> Option<&str> {
+    url.rfind('/').and_then(|last_slash| {
+        url.rfind(".tar.gz")
+            .map(|tar_gz_index| &url[last_slash + 1..tar_gz_index])
+    })
+}
+
 impl DistLayerMetadata {
-    fn current(layer: &DistLayer, context: &BuildContext<NodeJsEngineBuildpack>) -> Self {
+    fn current(layer: &DistLayer) -> Self {
         DistLayerMetadata {
-            nodejs_version: layer.release.version.to_string(),
+            artifact: layer.artifact.clone(),
             layer_version: String::from(LAYER_VERSION),
-            arch: context.target.arch.clone(),
-            os: context.target.os.clone(),
         }
     }
 }
