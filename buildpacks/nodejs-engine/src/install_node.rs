2177650c5a6b0d87e053e18546796443d21bbf6b
Colin Casey <casey.colin@gmail.com>
2024-07-18 10:17:16 -0300

    Update `nodejs-engine` to `libcnb@0.22` API (#880)
    * Update `nodejs-engine` to use new layer API

--- buildpacks/nodejs-engine/src/install_node.rs
+++ buildpacks/nodejs-engine/src/install_node.rs
@@ -1,27 +1,112 @@
-use crate::{NodeJsEngineBuildpack, NodeJsEngineBuildpackError};
+use std::fs;
+use std::io::Read;
+use std::path::Path;
+
 use heroku_inventory_utils::inv::Artifact;
-use heroku_nodejs_utils::vrs::Version;
 use libcnb::build::BuildContext;
-use libcnb::data::layer_content_metadata::LayerTypes;
-#[allow(deprecated)]
-use libcnb::layer::{ExistingLayerStrategy, Layer, LayerData, LayerResult, LayerResultBuilder};
-use libcnb::Buildpack;
+use libcnb::data::layer_name;
+use libcnb::layer::{
+    CachedLayerDefinition, InvalidMetadataAction, LayerState, RestoredLayerAction,
+};
 use libherokubuildpack::download::download_file;
 use libherokubuildpack::fs::move_directory_contents;
 use libherokubuildpack::log::log_info;
 use libherokubuildpack::tar::decompress_tarball;
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
-use std::fs;
-use std::io::Read;
-use std::path::Path;
 use tempfile::NamedTempFile;
 use thiserror::Error;
 
-/// A layer that downloads the Node.js distribution artifacts
-pub(crate) struct DistLayer {
-    pub(crate) artifact: Artifact<Version, Sha256>,
+use heroku_nodejs_utils::vrs::Version;
+
+use crate::{NodeJsEngineBuildpack, NodeJsEngineBuildpackError};
+
+pub(crate) fn install_node(
+    context: &BuildContext<NodeJsEngineBuildpack>,
+    distribution_artifact: &Artifact<Version, Sha256>,
+) -> Result<(), libcnb::Error<NodeJsEngineBuildpackError>> {
+    let new_metadata = DistLayerMetadata {
+        artifact: distribution_artifact.clone(),
+        layer_version: LAYER_VERSION.to_string(),
+    };
+
+    let distribution_layer = context.cached_layer(
+        layer_name!("dist"),
+        CachedLayerDefinition {
+            build: true,
+            launch: true,
+            invalid_metadata_action: &|_| InvalidMetadataAction::DeleteLayer,
+            restored_layer_action: &|old_metadata: &DistLayerMetadata, _| {
+                if old_metadata == &new_metadata {
+                    RestoredLayerAction::KeepLayer
+                } else {
+                    RestoredLayerAction::DeleteLayer
+                }
+            },
+        },
+    )?;
+
+    match distribution_layer.state {
+        LayerState::Restored { .. } => {
+            log_info(format!("Reusing Node.js {distribution_artifact}"));
+        }
+        LayerState::Empty { .. } => {
+            distribution_layer.write_metadata(new_metadata)?;
+
+            let node_tgz = NamedTempFile::new().map_err(DistLayerError::TempFile)?;
+
+            log_info(format!(
+                "Downloading Node.js {} from {}",
+                distribution_artifact, distribution_artifact.url
+            ));
+            download_file(&distribution_artifact.url, node_tgz.path())
+                .map_err(DistLayerError::Download)?;
+
+            log_info("Verifying checksum");
+            let digest = sha256(node_tgz.path()).map_err(DistLayerError::ReadTempFile)?;
+            if distribution_artifact.checksum.value != digest {
+                Err(DistLayerError::ChecksumVerification)?;
+            }
+
+            log_info(format!("Extracting Node.js {distribution_artifact}"));
+            decompress_tarball(&mut node_tgz.into_file(), distribution_layer.path())
+                .map_err(DistLayerError::Untar)?;
+
+            log_info(format!("Installing Node.js {distribution_artifact}"));
+
+            let dist_name = extract_tarball_prefix(&distribution_artifact.url)
+                .ok_or_else(|| DistLayerError::TarballPrefix(distribution_artifact.url.clone()))?;
+            let dist_path = distribution_layer.path().join(dist_name);
+            move_directory_contents(dist_path, distribution_layer.path())
+                .map_err(DistLayerError::Installation)?;
+        }
+    };
+
+    Ok(())
 }
+
+fn sha256(path: impl AsRef<Path>) -> Result<Vec<u8>, std::io::Error> {
+    let mut file = fs::File::open(path.as_ref())?;
+    let mut buffer = [0x00; 10 * 1024];
+    let mut sha256: Sha256 = Sha256::default();
+
+    let mut read = file.read(&mut buffer)?;
+    while read > 0 {
+        Digest::update(&mut sha256, &buffer[..read]);
+        read = file.read(&mut buffer)?;
+    }
+
+    Ok(sha256.finalize().to_vec())
+}
+
+fn extract_tarball_prefix(url: &str) -> Option<&str> {
+    url.rfind('/').and_then(|last_slash| {
+        url.rfind(".tar.gz")
+            .map(|tar_gz_index| &url[last_slash + 1..tar_gz_index])
+    })
+}
+
+const LAYER_VERSION: &str = "1";
 
 #[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Eq)]
 #[serde(deny_unknown_fields)]
@@ -48,105 +133,20 @@ pub(crate) enum DistLayerError {
     ReadTempFile(std::io::Error),
 }
 
-const LAYER_VERSION: &str = "1";
-
-#[allow(deprecated)]
-impl Layer for DistLayer {
-    type Buildpack = NodeJsEngineBuildpack;
-    type Metadata = DistLayerMetadata;
-
-    fn types(&self) -> LayerTypes {
-        LayerTypes {
-            build: true,
-            launch: true,
-            cache: true,
-        }
-    }
-
-    #[allow(deprecated)]
-    fn create(
-        &mut self,
-        _context: &BuildContext<Self::Buildpack>,
-        layer_path: &Path,
-    ) -> Result<LayerResult<Self::Metadata>, NodeJsEngineBuildpackError> {
-        let node_tgz = NamedTempFile::new().map_err(DistLayerError::TempFile)?;
-
-        log_info(format!(
-            "Downloading Node.js {} from {}",
-            self.artifact, self.artifact.url
-        ));
-        download_file(&self.artifact.url, node_tgz.path()).map_err(DistLayerError::Download)?;
-
-        log_info("Verifying checksum");
-        let digest = sha256(node_tgz.path()).map_err(DistLayerError::ReadTempFile)?;
-        if self.artifact.checksum.value != digest {
-            Err(DistLayerError::ChecksumVerification)?;
-        }
-
-        log_info(format!("Extracting Node.js {}", self.artifact));
-        decompress_tarball(&mut node_tgz.into_file(), layer_path).map_err(DistLayerError::Untar)?;
-
-        log_info(format!("Installing Node.js {}", self.artifact));
-
-        let dist_name = extract_tarball_prefix(&self.artifact.url)
-            .ok_or_else(|| DistLayerError::TarballPrefix(self.artifact.url.clone()))?;
-        let dist_path = Path::new(layer_path).join(dist_name);
-        move_directory_contents(dist_path, layer_path).map_err(DistLayerError::Installation)?;
-
-        LayerResultBuilder::new(DistLayerMetadata::current(self)).build()
-    }
-
-    #[allow(deprecated)]
-    fn existing_layer_strategy(
-        &mut self,
-        _context: &BuildContext<Self::Buildpack>,
-        layer_data: &LayerData<Self::Metadata>,
-    ) -> Result<ExistingLayerStrategy, <Self::Buildpack as Buildpack>::Error> {
-        if layer_data.content_metadata.metadata == DistLayerMetadata::current(self) {
-            log_info(format!("Reusing Node.js {}", self.artifact));
-            Ok(ExistingLayerStrategy::Keep)
-        } else {
-            Ok(ExistingLayerStrategy::Recreate)
-        }
-    }
-}
-
-fn sha256(path: impl AsRef<Path>) -> Result<Vec<u8>, std::io::Error> {
-    let mut file = fs::File::open(path.as_ref())?;
-    let mut buffer = [0x00; 10 * 1024];
-    let mut sha256: Sha256 = Sha256::default();
-
-    let mut read = file.read(&mut buffer)?;
-    while read > 0 {
-        Digest::update(&mut sha256, &buffer[..read]);
-        read = file.read(&mut buffer)?;
-    }
-
-    Ok(sha256.finalize().to_vec())
-}
-
-fn extract_tarball_prefix(url: &str) -> Option<&str> {
-    url.rfind('/').and_then(|last_slash| {
-        url.rfind(".tar.gz")
-            .map(|tar_gz_index| &url[last_slash + 1..tar_gz_index])
-    })
-}
-
-impl DistLayerMetadata {
-    fn current(layer: &DistLayer) -> Self {
-        DistLayerMetadata {
-            artifact: layer.artifact.clone(),
-            layer_version: String::from(LAYER_VERSION),
-        }
+impl From<DistLayerError> for libcnb::Error<NodeJsEngineBuildpackError> {
+    fn from(value: DistLayerError) -> Self {
+        libcnb::Error::BuildpackError(NodeJsEngineBuildpackError::DistLayerError(value))
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use std::str::FromStr;
+
     use heroku_inventory_utils::checksum::Checksum;
     use heroku_inventory_utils::inv::{Arch, Os};
-    use std::str::FromStr;
+
+    use super::*;
 
     #[test]
     fn dist_metadata_sanity_check() {
