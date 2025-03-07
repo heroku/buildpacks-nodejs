f0af81386942842cffc7d75c1063a5339193761e
Colin Casey <casey.colin@gmail.com>
2025-03-07 10:28:11 -0400

    Remove pack filters now that output goes to stderr

--- test_support/src/snapshot_filters.rs
+++ test_support/src/snapshot_filters.rs
@@ -9,8 +9,6 @@ pub(super) fn create_snapshot_filters(
     };
 
     let mut filters = vec![];
-    filters.extend(copy_filters(&PACK_FILTERS));
-    filters.extend(copy_filters(&BUILDPACK_FILTERS));
     filters.extend(copy_filters(&TIMER_FILTERS));
     filters.extend(copy_filters(&NPM_FILTERS));
     filters.extend(copy_filters(&PNPM_FILTERS));
@@ -20,62 +18,6 @@ pub(super) fn create_snapshot_filters(
     }
     filters
 }
-
-const PACK_FILTERS: [(&str, &str); 4] = [
-    (r"libcnbtest_\w+", "<build_image_name>"),
-    (r"\*\*\* Images \([\w\d]+\)", "*** Images (<image_hash>)"),
-    (
-        r"fail: ([\w/-]+)@\d+\.\d+\.\d+ provides unused \w+",
-        "fail: ${1}@<version> provides unused <provides_id>",
-    ),
-    (
-        r#"Restoring metadata for "(.*)" from app image"#,
-        r#"Restoring metadata for "<buildpack_id>" from app image"#,
-    ),
-];
-
-const BUILDPACK_FILTERS: [(&str, &str); 10] = [
-    (
-        r"heroku/nodejs-engine(\s+)\d+\.\d+\.\d+",
-        "heroku/nodejs-engine${1}<nodejs_engine_version>",
-    ),
-    (
-        r"heroku/nodejs-corepack(\s+)\d+\.\d+\.\d+",
-        "heroku/nodejs-corepack${1}<nodejs_corepack_version>",
-    ),
-    (
-        r"heroku/nodejs-yarn(\s+)\d+\.\d+\.\d+",
-        "heroku/nodejs-yarn${1}<nodejs_yarn_version>",
-    ),
-    (
-        r"heroku/nodejs-function-invoker(\s+)\d+\.\d+\.\d+",
-        "heroku/nodejs-function-invoker${1}<nodejs_function_invoker_version>",
-    ),
-    (
-        r"heroku/nodejs-npm(\s+)\d+\.\d+\.\d+",
-        "heroku/nodejs-npm${1}<nodejs_npm_version>",
-    ),
-    (
-        r"heroku/nodejs-pnpm-engine(\s+)\d+\.\d+\.\d+",
-        "heroku/nodejs-pnpm-engine${1}<nodejs_pnpm_engine_version>",
-    ),
-    (
-        r"heroku/nodejs-pnpm-install(\s+)\d+\.\d+\.\d+",
-        "heroku/nodejs-pnpm-install${1}<nodejs_pnpm_install_version>",
-    ),
-    (
-        r"heroku/nodejs-npm-engine(\s+)\d+\.\d+\.\d+",
-        "heroku/nodejs-npm-engine${1}<nodejs_npm_engine_version>",
-    ),
-    (
-        r"heroku/nodejs-npm-install(\s+)\d+\.\d+\.\d+",
-        "heroku/nodejs-npm-install${1}<nodejs_npm_install_version>",
-    ),
-    (
-        r"heroku/nodejs-yarn(\s+)\d+\.\d+\.\d+",
-        "heroku/nodejs-yarn${1}<nodejs_yarn_version>",
-    ),
-];
 
 const TIMER_FILTERS: [(&str, &str); 5] = [
     (
