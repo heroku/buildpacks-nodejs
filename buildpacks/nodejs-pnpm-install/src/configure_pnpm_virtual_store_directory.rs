a6c3c7b1773a753d42fe65eb33b25178b1e75ea4
Colin Casey <casey.colin@gmail.com>
2025-04-11 15:34:43 -0300

    Improve pnpm install errors
    This PR adds detailed and consistent error messages for the pnpm install buildpack + includes snapshot testing for the output.

--- buildpacks/nodejs-pnpm-install/src/configure_pnpm_virtual_store_directory.rs
+++ buildpacks/nodejs-pnpm-install/src/configure_pnpm_virtual_store_directory.rs
@@ -26,19 +26,23 @@ pub(crate) fn configure_pnpm_virtual_store_directory(
     log = log.sub_bullet("Creating pnpm virtual store");
     let virtual_store_dir = virtual_layer.path().join("store");
     // Create a directory for dependencies in the virtual store.
-    create_dir(&virtual_store_dir).map_err(PnpmInstallBuildpackError::VirtualLayer)?;
+    create_dir(&virtual_store_dir)
+        .map_err(|e| PnpmInstallBuildpackError::CreateDirectory(virtual_store_dir.clone(), e))?;
+
     cmd::pnpm_set_virtual_dir(env, &virtual_store_dir)
-        .map_err(PnpmInstallBuildpackError::PnpmDir)?;
+        .map_err(PnpmInstallBuildpackError::PnpmSetVirtualStoreDir)?;
 
     // Install a symlink from {virtual_layer}/node_modules to
     // {app_dir}/node_modules, so that dependencies in
     // {virtual_layer}/store/ can find their dependencies via the Node
     // module loader's ancestor directory traversal.
-    symlink(
-        context.app_dir.join("node_modules"),
-        virtual_layer.path().join("node_modules"),
-    )
-    .map_err(PnpmInstallBuildpackError::VirtualLayer)?;
+    let from = context.app_dir.join("node_modules");
+    let to = virtual_layer.path().join("node_modules");
+    symlink(&from, &to).map_err(|source| PnpmInstallBuildpackError::CreateSymlink {
+        from,
+        to,
+        source,
+    })?;
 
     Ok(log)
 }
