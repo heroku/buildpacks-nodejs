8d26f716f9b9690da23161cb5492d2b4821ff7e0
Colin Casey <casey.colin@gmail.com>
2025-04-17 17:32:45 -0300

    Output snapshots for pnpm engine (#1087)
    This PR replaces manual output assertions with snapshots for better visibility into output changes.

--- buildpacks/nodejs-pnpm-engine/tests/integration_test.rs
+++ buildpacks/nodejs-pnpm-engine/tests/integration_test.rs
@@ -1,9 +1,8 @@
 // Required due to: https://github.com/rust-lang/rust/issues/95513
 #![allow(unused_crate_dependencies)]
 
-use indoc::formatdoc;
-use libcnb_test::{assert_contains, PackResult};
-use test_support::nodejs_integration_test_with_config;
+use libcnb_test::PackResult;
+use test_support::{create_build_snapshot, nodejs_integration_test_with_config};
 
 #[test]
 #[ignore = "integration test"]
@@ -14,25 +13,7 @@ fn pnpm_unknown_version() {
             cfg.expected_pack_result(PackResult::Failure);
         },
         |ctx| {
-            assert_contains!(
-                ctx.pack_stderr,
-                &formatdoc! {"
-                    ! A pnpm lockfile (`pnpm-lock.yaml`) was detected, but the \
-                    version of `pnpm` to install could not be determined.
-                    !
-                    ! `pnpm` may be installed via the `heroku/nodejs-corepack` \
-                    buildpack. It requires the desired `pnpm` version to be set \
-                    via the `packageManager` key in `package.json`.
-                    !
-                    ! To set `packageManager` in `package.json` to the latest \
-                    `pnpm`, run:
-                    !
-                    ! `corepack enable`
-                    ! `corepack use pnpm@*`
-                    !
-                    ! Then commit the result, and try again.
-                "}
-            );
+            create_build_snapshot(&ctx.pack_stderr).assert();
         },
     );
 }
