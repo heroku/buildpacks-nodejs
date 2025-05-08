5d5979acc81e45b0dc28db58666c1656eb4c76ad
Colin Casey <casey.colin@gmail.com>
2025-05-08 16:39:19 -0300

    Remove functions-related buildpacks
    The `heroku/nodejs-function-invoker`, `heroku/nodejs-npm`, and `heroku/nodejs-function` buildpacks have been split out to their own [maintenance branch](https://github.com/heroku/buildpacks-nodejs/compare/v3.6.3...sf-functions). The reason for this is so that Node.js Function CNBs can be isolated from future changes to the buildpacks on `main`.

--- buildpacks/nodejs-function-invoker/opt/nodejs-runtime.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-runtime_bin=$1
-function_dir=$2
-
-exec $runtime_bin serve "$function_dir" --workers 2 --host "::" --port "${PORT:-8080}" --debug-port "${DEBUG_PORT:-}"
